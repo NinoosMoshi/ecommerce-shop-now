fd3e43570a8c6564c96c9b01965e861117b866fa
NinoosMoshi <nalkaton@gmail.com>
2025-07-23 08:33:33 -0500

    create entites

---
+++ backEnd/shop-now/src/main/java/com/ninos/model/Cart.java
@@ -0,0 +1,27 @@
+package com.ninos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
