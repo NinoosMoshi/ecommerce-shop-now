588aa16aab085c80a45faf1fe82b5baa7eb48459
NinoosMoshi <nalkaton@gmail.com>
2025-08-15 17:13:14 -0500

    add security

---
+++ backEnd/shop-now/src/main/java/com/ninos/security/user/ShopUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.ninos.security.user;
+
+import com.ninos.model.User;
+import com.ninos.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
